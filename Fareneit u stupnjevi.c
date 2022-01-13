40007199d16006445a890823cf4683676e745055
kenjac420 <95847529+kenjac420@users.noreply.github.com>
2022-01-13 07:46:53 +0100

    Add files via upload

---
+++ Fareneit u stupnjevi.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+void main(){
+
+int F,C,K;
+
+printf("unesi broj za Fareneite!\n");
+
+scanf("%d",&F);
+
+C=(F-32)\1.8
+K=C+273.15
+
+Printf("temperatura u celzijevim stupnjevima pomoć fareneita je %d d\n",C);
+printf("temperatura u kelvinima pomoću celzijevih stupnjeva je %d d\n",K)
+}
