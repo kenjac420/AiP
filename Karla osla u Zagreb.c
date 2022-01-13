1a5927953a500933d1254407ea2ae0fff9472ef8
kenjac420 <95847529+kenjac420@users.noreply.github.com>
2022-01-13 08:42:17 +0100

    Add files via upload

---
+++ Karla osla u Zagreb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main(){
+
+float O;
+int d;
+float n;
+
+scanf("%d",&d);
+
+n=3.1356;
+O=(float)d*n;
+
+float broj_okretaja=400000/O;
+
+printf("broj okretaja kotaca na 400 km je %.0f",broj_okretaja);
+
+}
